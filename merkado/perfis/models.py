7ea9b78103a094b80b69bb29f9ed52fcb922fef9
Leonardo <leovanzella@gmail.com>
2018-01-18 17:27:31 -0200

    3. Definig Perfil class

--- merkado/perfis/models.py
+++ merkado/perfis/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class Perfil(object):
+    def __init__(self, nome='', email='', telefone='', nome_empresa=''):
+        self.nome = nome
+        self.email = email
+        self.tefefone = telefone
+        self.empresa = nome_empresa
--- merkado/perfis/views.py
+++ merkado/perfis/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from perfis.models import Perfil
 
 def index(request):
     return render(request, 'index.html')
 
 def exibir(request, perfil_id):
+    perfil = Perfil()
+    if(perfil_id == '1'):
+        perfil = Perfil('Leonardo Vanzella', 'leovanzella@gmail.com', '777777', 'Oem')
     return render(request, 'perfil.html')
