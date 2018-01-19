4871023bd07d89b99a259f61ea590f33feb089da
Leonardo <leovanzella@gmail.com>
2018-01-19 10:26:42 -0200

    4. Replacing local instance for dbs. Need makemigrations

--- merkado/perfis/models.py
+++ merkado/perfis/models.py
@@ -1,10 +1,7 @@
 from django.db import models
 
-from django.db import models
-
-class Perfil(object):
-    def __init__(self, nome='', email='', telefone='', nome_empresa=''):
-        self.nome = nome
-        self.email = email
-        self.tefefone = telefone
-        self.empresa = nome_empresa
+class Perfil(models.Model):
+    nome = models.CharField(max_length=255, null=False)
+    email = models.CharField(max_length=255, null=False)
+    tefefone = models.CharField(max_length=15, null=False)
+    nome_empresa = models.CharField(max_length=255, null=False)
