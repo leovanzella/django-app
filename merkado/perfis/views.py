75ebbf4b2773c62bbc580fe72fa5626261bd2c09
Leonardo <leovanzella@gmail.com>
2018-01-22 17:35:03 -0200

    8. Preparing invitations and get logged profile

--- merkado/perfis/views.py
+++ merkado/perfis/views.py
@@ -3,12 +3,12 @@ from django.shortcuts import render, redirect
 from perfis.models import Perfil
 
 def index(request):
-    return render(request, 'index.html', {'perfis' : Perfil.objects.all()})
+    return render(request, 'index.html', {'perfis' : Perfil.objects.all(), 'perfil_logado' : get_perfil_logado(request)})
 
 def exibir(request, perfil_id):
     perfil = Perfil.objects.get(id=perfil_id)
     #perfil = Perfil.objects.filter(email__contains='Leo')  can search for other Leos
-    return render(request, 'perfil.html', {'perfil' : perfil})
+    return render(request, 'perfil.html', {'perfil' : perfil, 'perfil_logado' : get_perfil_logado(request)})
 
 def convidar(request, perfil_id):
     perfil_a_convidar = Perfil.objects.get(id=perfil_id)
