25d4e9878502dac4911fa64ed690814d90fd065a
Leonardo <leovanzella@gmail.com>
2018-01-22 10:43:38 -0200

    5. Added Invite methods

--- merkado/perfis/urls.py
+++ merkado/perfis/urls.py
@@ -1,8 +1,9 @@
-from perfis.views import index, exibir
+from perfis.views import *
 from django.urls import path, re_path
 
 urlpatterns = [
     path('',                  index, name='index'),
     path('perfis/',          exibir, name='exibir'),   #r'^perfis/\d+$'
     re_path(r'^perfis/(?P<perfil_id>\d+)$', exibir, name='exibir'),   #r'^perfis/\d+$'
+    re_path(r'^perfis/(?P<perfil_id>\d+)/convidar$', convidar, name='convidar'),
 ]
--- merkado/perfis/views.py
+++ merkado/perfis/views.py
@@ -8,3 +8,13 @@ def exibir(request, perfil_id):
     perfil = Perfil.objects.get(id=perfil_id)
     #perfil = Perfil.objects.filter(email__contains='Leo')  can search for other Leos
     return render(request, 'perfil.html', {'perfil' : perfil})
+
+def convidar(request, perfil_id):
+    pass
+    perfil_a_convidar = Perfil.objects.get(id=perfil_id)
+    perfil_logado = get_perfil_logado(request)
+    perfil_logado.convidar(perfil_a_convidar)
+    return render(request, 'index.html', {'perfis' : Perfil.objects.all()})
+
+def get_perfil_logado(request):
+    return Perfil.objects.get(id=1)
