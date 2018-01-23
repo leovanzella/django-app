5cae83f8827c1e787d54fb0fc8fbce9cb634c670
Leonardo A.  Vanzella <leovanzella@gmail.com>
2018-01-23 21:22:54 -0200

    10. Added forms for user registrations

--- merkado/usuarios/urls.py
+++ merkado/usuarios/urls.py
@@ -1,6 +1,6 @@
-from perfis.views import *
+from usuarios.views import *
 from django.urls import path, re_path
 
 urlpatterns = [
-    path(r'^registrar/$', ???, name = "registrar"),
+    re_path(r'^registrar/$', RegistrarUsuarioView.as_view(), name='registrar'),
 ]
