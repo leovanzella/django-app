e91cd54730c4b3724fe7be243b6df777969281e8
Leonardo A.  Vanzella <leovanzella@gmail.com>
2018-02-12 18:54:34 -0200

    Views login and logout has now Django's User authentication URLs

--- merkado/usuarios/urls.py
+++ merkado/usuarios/urls.py
@@ -1,6 +1,9 @@
 from usuarios.views import *
-from django.urls import path, re_path
+from django.conf.urls import re_path
+from django.contrib.auth.views import login, logout_then_login
 
 urlpatterns = [
     re_path(r'^registrar/$', RegistrarUsuarioView.as_view(), name='registrar'),
+    re_path(r'^login/$', login, {'template_name':'login.html'}, name='login'),
+    re_path(r'^logout/$', logout_then_login, {'login_url':'/login/'}, name='logout')
 ]
