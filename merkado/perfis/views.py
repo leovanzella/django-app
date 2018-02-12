0bc9d65bb67a82ce0453eefd32707b702466b9cc
Leonardo A.  Vanzella <leovanzella@gmail.com>
2018-02-12 18:57:34 -0200

    Added decorator @login_required for perfis views and return get_perfil_logado as request.user.perfil

--- merkado/perfis/views.py
+++ merkado/perfis/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render, redirect
 
 from perfis.models import Perfil, Convite
 
+from django.contrib.auth.decorators import login_required
+
+@login_required
 def index(request):
     return render(request, 'index.html', {'perfis' : Perfil.objects.all(), 'perfil_logado' : get_perfil_logado(request)})
 
+@login_required
 def exibir(request, perfil_id):
     perfil = Perfil.objects.get(id=perfil_id)
     #perfil = Perfil.objects.filter(email__contains='Leo')  can search for other Leos
@@ -12,14 +16,16 @@ def exibir(request, perfil_id):
     ja_eh_contato = perfil in perfil_logado.contatos.all()
     return render(request, 'perfil.html', {'perfil' : perfil, 'perfil_logado' : get_perfil_logado(request), 'ja_eh_contato' : ja_eh_contato})
 
+@login_required
 def convidar(request, perfil_id):
     perfil_a_convidar = Perfil.objects.get(id=perfil_id)
     perfil_logado = get_perfil_logado(request)
     perfil_logado.convidar(perfil_a_convidar)
     return redirect('index')
 
+@login_required
 def get_perfil_logado(request):
-    return Perfil.objects.get(id=1)
+    return request.user.perfil
 
 def aceitar(request, convite_id):
     convite = Convite.objects.get(id=convite_id)
