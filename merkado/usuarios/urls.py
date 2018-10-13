b8a5339ede67883f6f961e14f866fbe6d18d55ba
Leonardo Antonio Vanzella <leovanzella@gmail.com>
2018-10-13 09:20:56 -0300

    Changed login to LoginView according to last Django Version

--- merkado/usuarios/urls.py
+++ merkado/usuarios/urls.py
@@ -1,9 +1,9 @@
 from usuarios.views import *
 from django.conf.urls import re_path
-from django.contrib.auth.views import login, logout_then_login
+from django.contrib.auth.views import LoginView, logout_then_login
 
 urlpatterns = [
     re_path(r'^registrar/$', RegistrarUsuarioView.as_view(), name='registrar'),
-    re_path(r'^login/$', login, {'template_name':'login.html'}, name='login'),
+    re_path(r'^login/$', LoginView.as_view(template_name='login.html'), name='login'),
     re_path(r'^logout/$', logout_then_login, {'login_url':'/login/'}, name='logout')
 ]
